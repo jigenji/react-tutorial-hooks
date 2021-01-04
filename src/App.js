581a5d77e7b10d5cecc5b57a8f45757a35750b2f
Shinji Kawakami <skphysi929@gmail.com>
2021-01-04 11:58:16 +0900

    add the new components HookCounterThree to test the update method of dictionary state using hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import HookCounterThree from './components/HookCounterThree';
 // import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter'
+// import HookCounter from './components/HookCounter'
+// import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterThree />
     </div>
   );
 }
